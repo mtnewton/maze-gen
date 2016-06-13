16e3e9e47c43702b2cefd664bb67de232568ae32
Marvin Newton <mtnewton3@gmail.com>
2016-06-13 00:27:04 -0400

    Spacing. Canvas. Clicking generate updates canvas size.

--- js/maze-gen.js
+++ js/maze-gen.js
@@ -1,1 +1,45 @@
-$.material.init()
+$.material.init();
+var cDebug = $('#canvas-debug').get(0);
+var cOutput = $('#canvas-output').get(0);
+var ctxDebug;
+var ctxOutput;
+
+var options = {
+    width: $('#option-width'),
+    height: $('#option-height')
+};
+
+var settings = {
+    width: 100,
+    height: 100
+};
+
+function generate() {
+    console.log('generate()')
+    validateOptions();
+    updateSettings();
+    initCanvas()
+}
+
+function validateOptions(){
+    console.log('validateOptions()')
+}
+
+function updateSettings(){
+    console.log('updateSettings()');
+    settings.width = parseInt(options.width.val());
+    settings.height = parseInt(options.height.val());
+    console.log("Settings: " + JSON.stringify(settings));
+}
+
+function initCanvas() {
+    console.log('initCanvas()');
+
+    cDebug.width = settings.width;
+    cDebug.height = settings.height;
+    ctxDebug = cDebug.getContext("2d");
+
+    cOutput.width = settings.width;
+    cOutput.height = settings.height;
+    ctxOutput = cOutput.getContext("2d");
+}
